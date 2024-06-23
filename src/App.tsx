cc38eaebba236f0110395a12e6bd4d35c8f72607
georgii <rsnzwgahl@proton.me>
2024-06-23 20:54:53 +0300

    Initial deployment of PWA

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App fullscreen">
+      <header className="App-header">
+        <h1>Welcome to My PWA</h1>
+      </header>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/index.tsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
+import * as serviceWorkerRegistration from './serviceWorkerRegistration';
+import reportWebVitals from './reportWebVitals';
+
+ReactDOM.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById('root')
+);
+
+// Регистрируем Service Worker
+serviceWorkerRegistration.register();
+
+reportWebVitals();
